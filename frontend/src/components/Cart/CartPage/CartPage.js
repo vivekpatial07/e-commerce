25d2686b92f62930f81752ad3cfbad5f1c255588
vivek <vivek.patial@hoolime.co>
2021-06-22 15:12:07 +0530

    guest cart added

--- frontend/src/components/Cart/CartPage/CartPage.js
+++ frontend/src/components/Cart/CartPage/CartPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCartItemsInitiate } from '../../../redux/actionCreators/cartCreators'
 import { allCartData } from '../../../redux/selectors/cartSelector'
@@ -8,16 +8,32 @@ import CommonButton from '../../BuyButton/CommonButton'
 import ProductInCart from './ProductInCart/ProductInCart'
 import './CartPage.css'
 
-const CartPage = () => {
+const CartPage = ({ history }) => {
   
+  // const [totalPrice, setTotalPrice] = useState()
   const dispatch = useDispatch()
   const { cartItems, cartItemLoader } = useSelector(allCartData)
-  
   useEffect(() => {
     dispatch(getCartItemsInitiate())
   }, [dispatch])
 
+  
+  const buyNowHandler = () => {
+    history.push('/ecommerce/placeOrder')
+  }
+  
+  
+  console.log(cartItems)
   const productsInCart = cartItems.map((itm) =>  <ProductInCart product={itm} key={itm._id}/>)
+  let totalprice = 0
+
+  totalprice = cartItems.reduce((acc, curr) => {
+    return acc + Number(curr.price*curr.qty)
+  },0)
+  
+  
+
+  // console.log(totalPrice)
 
   return (
     <>
@@ -37,8 +53,8 @@ const CartPage = () => {
               </div>
               <div className='cartPageRightInfo'>
               <h2>Price</h2>
-                $77
-                <CommonButton btnClass='card-buy-now'>Buy Now</CommonButton>
+                ${totalprice.toFixed(2)}
+                <CommonButton btnClass='card-buy-now' onClick={buyNowHandler}>Buy Now</CommonButton>
                 <CommonButton btnClass='card-add-to-cart'>Add to wishlist</CommonButton>
               </div>
               </div>
--- frontend/src/components/Cart/CartPage/ProductInCart/ProductInCart.js
+++ frontend/src/components/Cart/CartPage/ProductInCart/ProductInCart.js
@@ -1,13 +1,82 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getCartItemsInitiate } from '../../../../redux/actionCreators/cartCreators'
 import CounterBtn from '../../../CounterBtn/CounterBtn'
 import './ProductInCart.css'
 
 const ProductInCart = ({ product: { _id, qty, image, price, title, category }}) => {
 
+  const [quantity, setQuantity] = useState(qty)
+
+  const dispatch = useDispatch()
+
   useEffect(() => {
     console.log('running')
   }, [])
 
+  const counterHandler = async(e) => {
+    const cartItems =  JSON.parse(localStorage.getItem('cartInfo'))
+
+    if(e.target.innerText === '+') {
+      setQuantity(quantity+1)
+      // dispatch(  )
+
+    if(cartItems) {
+      const itmIndex = cartItems.findIndex(itm => itm.id===_id)
+
+      if(itmIndex === -1) {
+        cartItems.push({id: _id,qty:1})
+      } else {
+        cartItems[itmIndex].qty+=1 
+      }
+      console.log(cartItems)
+      localStorage.setItem('cartInfo', JSON.stringify(cartItems))
+      dispatch(getCartItemsInitiate())
+
+    } else {
+      const cartItems = []
+      cartItems.push({id:_id, qty:1})
+      console.log(cartItems)
+    
+      
+      dispatch(getCartItemsInitiate())
+      localStorage.setItem('cartInfo', JSON.stringify(cartItems))
+    }
+    } else {
+      if(quantity>1) setQuantity(quantity-1)
+
+      if(cartItems) {
+        const itmIndex = cartItems.findIndex(itm => itm.id===_id)
+  
+        if(itmIndex !== -1 && cartItems.length > 0 ) {
+          if(cartItems[itmIndex].qty === 1) {
+
+
+          } else {
+
+            
+            cartItems[itmIndex].qty-=1 
+            localStorage.setItem('cartInfo', JSON.stringify(cartItems))
+            dispatch(getCartItemsInitiate())
+          }
+      
+      } 
+        // const cartItems = []
+        // cartItems.push({id:_id, qty:0})
+        // console.log(cartItems)
+        //do nothing
+      
+        console.log('no product added')
+        localStorage.setItem('cartInfo', JSON.stringify(cartItems))
+      
+    
+    }
+  }
+}
+
+  const changeHandler = (e) => {
+    e.target.value = quantity
+  }
 
   return (
     <div className='productInCartWrapper'>
@@ -17,9 +86,9 @@ const ProductInCart = ({ product: { _id, qty, image, price, title, category }}) 
       </div>
       <div className='productInCartCounterWrapper'>
         <div className='productInCartCounter'>
-          <CounterBtn btnClass='counterBtn'>-</CounterBtn>
-          <input className='productInCartInput' value={qty}/>
-          <CounterBtn btnClass='counterBtn' >+</CounterBtn>
+          <CounterBtn btnClass='counterBtn' onClick={counterHandler}>-</CounterBtn>
+          <input className='productInCartInput' value={quantity} onChange={changeHandler}/>
+          <CounterBtn btnClass='counterBtn' onClick={counterHandler}>+</CounterBtn>
         </div>
         <div className='productInCartPrice'>${price}</div>
       </div>
