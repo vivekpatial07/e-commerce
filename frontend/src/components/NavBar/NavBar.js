169d67a1ae60c6cc1c0f257fa2eb85a3819a138a
vivek <vivek.patial@hoolime.co>
2021-06-30 22:27:04 +0530

    nav-bar restyling started

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,60 +1,50 @@
-// import React, { useState } from 'react'
-// import { useDispatch } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-// import { logoutInitiate } from '../../redux/actionCreators/authCreators'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './NavBar.css'
-// import { useDispatch } from 'react-redux'
 
 
 const NavBar = ({history}) => {
   const user = JSON.parse(localStorage.getItem('userInfo'))
-  // const dispatch = useDispatch()
-  // const [cartCounter, setCartCounter] = useState()
-  // const dispatch = useDispatch()
+
   const cartItems = JSON.parse(localStorage.getItem('cartInfo'))
 
   const num = cartItems && cartItems.reduce((acc, curr) => acc+curr.qty,0)
 
-  // const [showDrop, setShowDrop] = useState(false)
-  // const logoutHandler = () => {
-  //   dispatch(logoutInitiate())
-  // }
-
-  // const drop = (
-  //   <div className='dropdown'>
-  //     hi
-  //   </div>
-  // )
-
   return (
     <div className='nav-wrapper'>
-      <div className='logo' onClick={()=>history.push('/')}>
-        A
-      </div>
-      <div className='left-items'>
-        <div 
-          // onClick={() => setShowDrop(!showDrop)}
-        >
-          {user?.username
-            ? (
-              <div
-          onClick={() => history.push('/ecommerce/user/profile')}
-               
-               >
-                <FontAwesomeIcon icon={faUser}/>
-                {/* {showDrop && drop} */}
-               </div>
-            )
-            : <div onClick={()=>{
-                // dispatch()
-                history.push('/login')
-              }}>Login</div>}
+      <div className='nav-top'>
+        <div className='nav-top-left'>
+          <div className='ham-container'>
+            <div className='ham'></div>
+            <div className='ham'></div>
+            <div className='ham'></div>
+          </div>
+        <div className='logo' onClick={()=>history.push('/')}>
+          A
         </div>
+        </div>
+        <div className='right-items'>
+          <div >
+            {user?.username
+              ? (
+                <div onClick={() => history.push('/ecommerce/user/profile')}>
+                  <FontAwesomeIcon icon={faUser}/>
+                </div>
+              )
+              : <div onClick={()=>{
+                  history.push('/login')
+                }}>Login</div>}
+          </div>
         <div onClick={()=> history.push('/ecommerce/cart')}>
           <FontAwesomeIcon style={{position:'relative'}} icon={faShoppingCart}/><span style={{position: 'absolute', top:'10px'}}>{num}</span>
+        </div>
+      </div>
+      </div>
+      <div className='nav-bottom'>
+        <div>
+          <input placeholder='search products'/>
         </div>
       </div>
     </div>
